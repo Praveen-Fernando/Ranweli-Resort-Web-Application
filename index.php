2ea1966af2ae01040c76b65fb1937d4724b13e3b
dewmalFernando <dewmalfernando@gmail.com>
2020-09-28 21:10:23 +0530

    Commit 9 - Dew

--- index.php
+++ index.php
@@ -1,26 +1,187 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <link rel="stylesheet" type="text/css" href="css/index-css/style.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-	
-	<!-- Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-	<!-- Font awesome -->
-	<link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
-
-	<!-- Style sheet -->
-	<link rel="stylesheet" href="css/style.css">
-
-	<title>Document</title>
 </head>
+
 <body>
-<h1>Ranweli Resort</h1>
+    <div class="loader-container">
+        <div class="loader"></div>
+    </div>
+    <!-- Header Start -->
+    <input type="checkbox" name="mobile-nav" id="mobile-nav" class="gaadiexp-check" onchange="this.blur()">
+    <label for="mobile-nav" class="gaadiexp white" tabindex="0"><span></span></label>
+    <nav role="navigation" class="header-nav">
+
+        <div class="menu" style="margin-left: -16px;">
+            <ul style="width: 267px; text-align: start;">
+
+                <li class="page_item page-item-2">
+                    <a href="index.php" class="list-group-item">Home</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="profile.html" class="list-group-item ">Activities</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="wishlist.html" class="list-group-item">The Resort</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="wishlist.html" class="list-group-item">Accommodation</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="interest.html" class="list-group-item">Environmental Policies</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="bids.html" class="list-group-item">Ayurveda</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Weedings</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Reservations</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="bid-history.html" class="list-group-item">Our Partners</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Awards & Recognition</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Gallery</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">What's New</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Special Offers</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">About us</a>
+                </li>
+                <li class="page_item page-item-2">
+                    <a href="dashboard.html" class="list-group-item">Contact Us</a>
+                </li>
+
+            </ul>
+        </div>
+
+    </nav>
+
+    <div class="slideshow-container1">
+        <div class="mySlides fade1">
+            <img src="images/index-imgs/ranweli_header1.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade1">
+            <img src="images/index-imgs/ranweli_header2.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade1">
+            <img src="images/index-imgs/ranweli_header3.jpg" style="width:100%">
+        </div>
+
+        <div style="text-align:center; margin-top: -50px;">
+            <div>
+
+                <body onload="startTime()" style="text-align: center; margin-top: -50px; font-size: 37px; "></body>
+                <div id="txt" style="color: white;"></div>
+            </div>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+
+            <div class="navbar1" style="background-color: #ffffff42;  position: fixed; ">
+                <div style="margin-left: 95px;">
+                    <a href="#home" style="background-color: ##ffffffe0; float: left;">Accommodation</a>
+                    <a href="#news" style="background-color: ##ffffffe0; float: left;">Dining</a>
+                    <a href="#contact" style="background-color: ##ffffffe0; float: left;">Offers</a>
+
+                    <a href="#book now" style="background-color: #dc3545; float: right;">Book Now</a>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+
+
+        <!-- Page Body Start -->
+
+
+
+        <!-- Type your code here -->
+
+
+
+
+        <!-- Page Body End -->
+
+
+
+        <!-- Footer Start -->
+        <?php include "./footer.php" ?>
+        <!-- Footer End -->
+
+        <!-- JavaScripts -->
+        <script>
+            var slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {
+                    slideIndex = 1
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex - 1].style.display = "block";
+                dots[slideIndex - 1].className += " active";
+                setTimeout(showSlides, 10000); // Change image every 10 seconds
+            }
+
+            //Clock
+            function startTime() {
+                var today = new Date();
+                var h = today.getHours();
+                var m = today.getMinutes();
+                var s = today.getSeconds();
+                m = checkTime(m);
+                s = checkTime(s);
+                document.getElementById('txt').innerHTML =
+                    h + ":" + m;
+                var t = setTimeout(startTime, 500);
+            }
+
+            function checkTime(i) {
+                if (i < 10) {
+                    i = "0" + i
+                }; // add zero in front of numbers < 10
+                return i;
+            }
+
+
+            $(window).on("load", function() {
+                $(".loader-container").fadeOut(1000);
+            });
+
+
+            $(window).load(function() {
+                loadClock("clock");
+            });
+        </script>
 </body>
+
 </html>
---
+++ js/index-js/accommodation.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides1(slideIndex);
+
+function plusSlides(n) {
+  showSlides1(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides1(slideIndex = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
